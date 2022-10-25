937f1c641eae62c0e18821eee996e325de3c87a5
Nthokar <zhvl03@gmail.com>
2022-10-26 01:04:03 +0500

    add base dish db api
    add dish get, get(id), post, put routing

---
+++ wepAplicatiob/wepAplicatiob/Controllers/DishesControler.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace wepAplicatiob.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DishesControler : ControllerBase
+    {
+        DbContext db;
+
+        private readonly ILogger<DishesControler> _logger;
+
+        public DishesControler(ILogger<DishesControler> logger, DbContext context)
+        {
+            db = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Dish>>> Get()
+        {
+            return await db.Dishes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Dish>> Get(Guid id)
+        {
+            Dish dish = await db.Dishes.FirstOrDefaultAsync(x => x.Id == id);
+            if (dish == null)
+                return NotFound();
+            return new ObjectResult(dish);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Dish>> Post(Dish dish)
+        {
+            if (dish == null)
+            {
+                return BadRequest();
+            }
+            db.Dishes.Add(dish);
+            await db.SaveChangesAsync();
+            return Ok(dish);
+        }
+        [HttpPut]
+        public async Task<ActionResult<Dish>> Put(Dish dish)
+        {
+            if (dish == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Dishes.Any(x => x.Id ==dish.Id))
+            {
+                return NotFound();
+            }
+
+            db.Update(dish);
+            await db.SaveChangesAsync();
+            return Ok(dish);
+        }
+    }
+}
---
+++ wepAplicatiob/wepAplicatiob/DbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace wepAplicatiob;
+using Microsoft.EntityFrameworkCore;
+
+public class DbContext: Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbSet<Dish> Dishes { get; set; }
+    public DbContext(DbContextOptions<DbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Dish>()
+            .HasKey(d => d.Id)
+            .HasName("PK_DishId");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=192.168.1.2;Port=5432;Database=usersdb;Username=postgres;Password=mysecretpassword");
+    }
+}
