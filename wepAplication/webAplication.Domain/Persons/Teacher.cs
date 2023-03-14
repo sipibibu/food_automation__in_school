1759139a7ac994209374fdc3eaf77c6befe0dcf1
StumpyTax <104196692+StumpyTax@users.noreply.github.com>
2023-03-14 21:03:31 +0500

    teacher

--- wepAplication/webAplication.Domain/Persons/Teacher.cs
+++ wepAplication/webAplication.Domain/Persons/Teacher.cs
@@ -6,6 +6,16 @@ namespace webAplication.Domain.Persons
     {
         public Teacher(string role, string name) : base(role, name) { }
         public Teacher(TeacherEntity entity):base(entity) { }
+        public override TeacherEntity ToEntity()
+        {
+            return new TeacherEntity()
+            {
+                Id = this.Id,
+                Name = this.name,
+                Role = this.role,
+                ImageId = this.imageId
+            };
+        }
         public void Update(Teacher teacher)
         {
             this.name = teacher.name;
