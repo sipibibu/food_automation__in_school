d7df91514cf68901f3f37823a1910b57e4b15d5e
Nthokar <87079516+Nthokar@users.noreply.github.com>
2023-04-11 17:24:15 +0500

    :+1:

---
+++ wepAplication/webAplication.Service/Models/JsonModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DataType = OfficeOpenXml.FormulaParsing.ExpressionGraph.DataType;
+
+namespace webAplication.Service.Models;
+
+public class JsonModel
+{
+    [Required]
+    //[DataType(DataType)]
+    public string text { get; set; }
+}
